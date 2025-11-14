d7a1bc922530242ac1515d7c9345a8d1fd66857b
Luiscamara123 <luis.alcantaracamara@gmail.com>
2025-11-14 16:05:03 -0300

    Mudei a mensagem

--- Calculadora-Desconto/js/script.js
+++ Calculadora-Desconto/js/script.js
@@ -14,20 +14,27 @@ function calcularValorFinal () {
     let valorFinal = valor;
     let classe = "";
 
+    let mensagem = "";
+
+
     if (forma === "avista") {
         valorFinal *= 0.9;
         classe = "verde";
+        mensagem = "À Vista";
     } else if (forma === "cartao") {
         valorFinal *= 1.05;
         classe = "vermelho";
+        mensagem = "Cartão";
     } else if (forma === "2x") {
         valorFinal = valor;
         classe = "laranja";
+        mensagem = "Parcelado em 2x";
     } else if (forma === "3xoumais") {
         valorFinal *= 1.10;
         classe = "vermelho";
+        mensagem = "Parcelado em 3x ou mais";
     }
 
-    resultadoDiv.innerHTML = `Valor final: R$ ${valorFinal.toFixed(2)}`;
+    resultadoDiv.innerHTML = `Valor final: R$ ${valorFinal.toFixed(2)} ${mensagem}`;
     resultadoDiv.className = classe;
 }
